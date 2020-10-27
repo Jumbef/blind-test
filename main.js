1735a9553ba8c0ac4138f38aff6daa5aea2f42c0
Jean-Baptiste FAUCHERON (Jumbef) <jb.faucheron@gmail.com>
2020-10-27 16:22:40 +0100

    Factorize IpcMain commands code

--- main.js
+++ main.js
@@ -66,59 +66,76 @@ app.whenReady().then(() => {
 })
 
 
-ipcMain.on('exit', (evt, arg) => {
-  app.exit()
-})
-ipcMain.on('load', (evt, arg) => {
-  winBack.webContents.send('stop')
-  winFront.webContents.send('stop')
-  winBack.webContents.send('load', playlist[playlistIndex][0])
-  winFront.webContents.send('load', playlist[playlistIndex][1])
-  playlistIndex++
-  if (playlistIndex >= playlist.length) { playlistIndex = 0 }
-})
-ipcMain.on('play', (evt, arg) => {
-  winBack.webContents.send('play')
-  winFront.webContents.send('play')
-})
-ipcMain.on('stop', (evt, arg) => {
-  winBack.webContents.send('stop')
-  winFront.webContents.send('stop')
-})
-ipcMain.on('pause', (evt, arg) => {
-  winBack.webContents.send('pause')
-  winFront.webContents.send('pause')
-})
-ipcMain.on('progress', (evt, arg) => {
-  console.log(arg)
-})
-ipcMain.on('resetbuzz', (evt, arg) => {
-  buzz = false
-  winBack.webContents.send('buzz', null)
-  winFront.webContents.send('buzz', null)
-  console.log("Buzzer reset")
-})
+ipcMain.on('command', async (evt, arg) => {
+  switch (arg.name) {
+    case 'exit':
+      app.exit()
+      break
+    case 'load':
+      winBack.webContents.send('stop')
+      winFront.webContents.send('stop')
+      winBack.webContents.send('load', playlist[playlistIndex][0])
+      winFront.webContents.send('load', playlist[playlistIndex][1])
+      playlistIndex++
+      if (playlistIndex >= playlist.length) { playlistIndex = 0 }
+      break
+    case 'play':
+      winBack.webContents.send('play')
+      winFront.webContents.send('play')
+      break;
+    case 'stop':
+      winBack.webContents.send('stop')
+      winFront.webContents.send('stop')
+      break
+    case 'pause':
+      winBack.webContents.send('pause')
+      winFront.webContents.send('pause')
+      break
+    case 'progress':
+      console.log(arg)
+      break
+    case 'resetbuzz':
+      buzz = false
+      winBack.webContents.send('buzz', null)
+      winFront.webContents.send('buzz', null)
+      console.log("Buzzer reset")
+      break
+    case 'open-link':
+      shell.openExternal(arg.value)
+      break
 
-ipcMain.on('open-link', (evt, arg) => {
-  shell.openExternal(arg)
-})
-
-
-ipcMain.on('folder-select', async (event, arg) => {
-  const result = await dialog.showOpenDialog(winBack, {
-    properties: ['openDirectory']
-  })
-  console.log('folder selected', result.filePaths)
-  if (!result.canceled) {
-    winBack.webContents.send('folder-selected', result.filePaths[0])
-    fs.readdir(result.filePaths[0], (err, dir) => {
-      let i = 0
-      for (let filePath of dir) {
-        winBack.webContents.send('add-file', [`tilte-${i}`, `${result.filePaths[0]}/${filePath}`, filePath])
-        playlist[i] = [`tilte-${i}`, `${result.filePaths[0]}/${filePath}`, filePath]
-        console.log(filePath)
-        i++
+    case 'folder-select':
+      const result = await dialog.showOpenDialog(winBack, {
+        properties: ['openDirectory']
+      })
+      console.log('folder selected', result.filePaths)
+      if (!result.canceled) {
+        winBack.webContents.send('folder-selected', result.filePaths[0])
+        fs.readdir(result.filePaths[0], (err, dir) => {
+          let i = 0
+          for (let filePath of dir) {
+            winBack.webContents.send('add-file', [`tilte-${i}`, `${result.filePaths[0]}/${filePath}`, filePath])
+            playlist[i] = [`tilte-${i}`, `${result.filePaths[0]}/${filePath}`, filePath]
+            console.log(filePath)
+            i++
+          }
+        });
       }
-    });
+      break
+
+    default:
+      break;
+  }
+})
+
+
+ipcMain.on('event', (evt, arg) => {
+  switch (arg.name) {
+    case 'progress':
+      console.log(arg.params[0])
+      break;
+
+    default:
+      break;
   }
 })
